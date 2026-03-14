1f2f83ab57c3b82637797c3344dcdece5f34163e
UIVsauce <>
2026-03-14 23:59:36 +0000

    Challenge: Event Handler with Lambda

---
+++ best_practices_and_real_world_applications/challenge_event_handler_with_lambda/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Consumer<String> messageHandler = message -> System.out.println("Notification: New message received - " + message);
+
+        handleNewMessage("Hello, you have a new message!", messageHandler);
+    }
+
+    public static void handleNewMessage(String message, Consumer<String> onMessage) {
+        onMessage.accept(message);
+    }
+}
